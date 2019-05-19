c5937d997295186ee189b70218544f5364f6c09e
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-19 19:50:16 +0600

    Implementation

---
+++ B - YYMM or MMYY.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define dbg(x)                cout <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+void FAST_IO();
+void ara_read(vec &v,ll n);
+////////////////////////
+
+int main()
+{
+	
+	string s;
+	cin>>s;
+	ll first=(s[0]-'0')*10+(s[1]-'0');
+	ll second=(s[2]-'0')*10+(s[3]-'0');
+	ll a=false, b=false;
+	if(first<=12 && first>0)
+		a=true;
+	if(second<=12 && second>0)
+		b=true;
+	if(a && b){
+		puts("AMBIGUOUS");
+	}
+	else if(a){
+		puts("MMYY");
+	}else if(b){
+		puts("YYMM");
+	}else{
+		puts("NA");
+	}
+	
+   return 0;
+}
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+
+}
+void ara_read(vec &v,ll n){
+	ll temp;
+	for(ll i=0; i<n; i++){
+		scanf("%lld",&temp);
+		v.emplace_back(temp);
+	}
+	
+}
