2dbc39ff2d99837124ac623729414fdf1c0e65d2
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-01-09 00:52:56 +0600

    Math

---
+++ A - Blackjack .cpp
@@ -0,0 +1,11 @@
+				/*Saurav Paul*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a , b , c;
+	cin >> a >> b >> c;
+	cout << (a+b+c>=22 ? "bust":"win") << endl ;
+    return 0;
+}
