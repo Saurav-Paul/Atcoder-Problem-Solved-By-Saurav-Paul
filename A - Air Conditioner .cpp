42b8ae803a00c89a840de4ca248d8a375f2c4196
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-08-02 21:11:53 +0600

    Create A - Air Conditioner .cpp

---
+++ A - Air Conditioner .cpp
@@ -0,0 +1,23 @@
+/**
+ *    author        : Saurav Paul 
+ *    created       : August 02, 2020 6:00 PM
+ *    Problem Name  : A - Air Conditioner
+ *    Problem Limit : 2000 ms , 1024 MB
+ *    Problem Url   : https://atcoder.jp/contests/abc174/tasks/abc174_a
+ *    @genarated by : ai-virtual-assistant
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int x ;
+    cin >> x;
+
+    cout << (x >= 30? "Yes" : "No") << endl ;
+
+
+    return 0 ;
+}
